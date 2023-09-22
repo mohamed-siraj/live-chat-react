43b0b19d6e9febb30bfaffabdbb3f1608c2f7112
Mohamed Siraj <siraj@oganro.com>
2023-09-23 01:02:32 +0530

    chat bot box first half created

---
+++ src/assets/img/index.tsx
@@ -0,0 +1,30 @@
+import EMailFull from './email-full.png';
+import EMail from './email.png';
+import ProfileS from './profile.png';
+import UserFUllS from './user-full.png';
+import UserE from './user.png';
+
+export const EmailFull = {
+    img: EMailFull, //note the default part at the end
+    alt: 'email-full.png'
+};
+
+export const Email = {
+    img: EMail, //note the default part at the end
+    alt: 'email.png'
+};
+
+export const Profile = {
+    img: ProfileS, //note the default part at the end
+    alt: 'profile.png'
+};
+
+export const UserFUll = {
+    img: UserFUllS, //note the default part at the end
+    alt: 'user-full.png'
+};
+
+export const User = {
+    img: UserE, //note the default part at the end
+    alt: 'user.png'
+};
--- src/pages/chat-box/index.tsx
+++ src/pages/chat-box/index.tsx
@@ -1,20 +1,42 @@
+import { Email, Profile, User } from "assets/img";
 import { SirajSvg } from "assets/svg";
+import RoundBtn from "components/buttons/roundBtn";
 import React from "react";
 
 const ChatBox: React.FunctionComponent = () => {
 
     return (<>
-        <div className="flex justify-center py-48 ">
-            <div className="rounded-l-2xl shadow-2xl bg-white p-8 ">
+        <div className="flex justify-center py-28 ">
+            <div className="rounded-l-2xl shadow-2xl bg-white p-8 chat-box" >
                 <div className="flex flex-row">
-                    <img src={SirajSvg.img} alt={SirajSvg.alt} className="w-16"/>
+                    <img src={SirajSvg.img} alt={SirajSvg.alt} className="w-16" />
                     <div className="text-purple-800 text-xl ml-3 mt-4"><strong>Siraj Chat Bot</strong></div>
                 </div>
-            </div>
-            <div className="shadow-2xl bg-white p-8 ">
+                <div className="flex flex-row mt-7">
+                    <img src={Email.img} alt={Email.alt} className="w-9 h-9" />
+                    <div className="text-md ml-3 mt-1.5"><strong>Messages</strong></div>
+                </div>
+                <div className="flex flex-row mt-7">
+                    <img src={User.img} alt={User.alt} className="w-9 h-9" />
+                    <div className="text-md ml-3 mt-1.5"><strong>Profile</strong></div>
+                </div>
+
+                <div className="flex justify-center mt-96">
+                        <RoundBtn name="Twit" type="button"/>
+                </div>
+                <div className="flex flex-row mt-7">
+                    <img src={Profile.img} alt={Profile.alt} className="w-10 h-10" />
+                    <div className="text-md ml-3 mt-1.5">
+                        <strong>Mohamed Siraj</strong><br/>
+                        <span className="text-md">@amsiraj94@gmail.com</span>
+                    </div>
+                </div>
 
             </div>
-            <div className="rounded-r-2xl shadow-2xl bg-white p-8 ">
+            <div className="shadow-2xl bg-white p-8 chat-box">
+
+            </div>
+            <div className="rounded-r-2xl shadow-2xl bg-white p-8  chat-box">
 
             </div>
         </div>
